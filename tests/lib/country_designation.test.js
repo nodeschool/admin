33f9247d4113e07064c56059ff6ebc61f268b224
fforres <felipe.torressepulveda@gmail.com>
2016-01-23 18:05:10 -0300

    a

--- tests/lib/country_designation.test.js
+++ tests/lib/country_designation.test.js
@@ -2,12 +2,40 @@ var CountryDesignation = require('../../lib/country_designations.js')
 var chai = require('chai')
 var expect = chai.expect
 
-describe('Country desgnations', () => {
-  it('should get list of areas', () => {
-    return expect(Geocode.country('Zagreb', 'HR')).to.eventually.contain(pos)
+describe('Country designations', () => {
+  describe('areas', () => {
+    it('should return an array with elements', () => {
+      return expect(CountryDesignation.areas).to.be.instanceof(Array).and.to.not.be.empty
+    })
+
+    it('should return an array with the areas', () => {
+      const Areas = [
+        'Latin America',
+        'South Pacific',
+        'North America',
+        'Asia',
+        'Africa',
+        'Europe',
+        'Middle East',
+        'Southpole'
+      ]
+      return expect(CountryDesignation.areas).to.eql(Areas)
+    })
   })
-  it('should find lat lng', () => {
-    var pos = { lat: 45.8150108, lng: 15.981919 }
-    return expect(Geocode.latlng('Zagreb, HR')).to.eventually.become(pos)
+
+  describe('prettyCountriesListByArea', () => {
+    it('should return a function', () => {
+      return expect(CountryDesignation.prettyCountriesListByArea).to.be.instanceof(Function)
+    })
+    it('should return an array with the "<Code> - <County>" syntax', () => {
+      const prettyList = [
+        'AQ - Antarctica',
+        'BV - Bouvet Island',
+        'TF - French Southern Territories',
+        'HM - Heard Island and McDonald Islands',
+        'GS - South Georgia and the South Sandwich Islands'
+      ]
+      return expect(CountryDesignation.prettyCountriesListByArea('Southpole')).to.be.instanceOf(Array).and.to.eql(prettyList)
+    })
   })
 })
