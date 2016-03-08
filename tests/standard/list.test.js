e72056b3a591cc503b04f1d9fe1a57deefbbb261
Martin Heidegger <martin.heidegger@gmail.com>
2016-03-08 22:43:00 +0900

    Adjusted tests to fit the changed implementation details

--- tests/standard/list.test.js
+++ tests/standard/list.test.js
@@ -1,5 +1,4 @@
 var List = require('../../lib/standard/list')
-var Github = require('../../lib/util/github')
 var Json = require('../../lib/util/json')
 var Validate = require('../../lib/util/validate')
 var sinon = require('sinon')
@@ -16,18 +15,51 @@ describe('Standard List', () => {
     chapter: 'zagreb',
     branch: 'gh-pages'
   }
-  var chapter = {
-    name: 'NodeSchool Zagreb',
-    location: {
-      name: 'Zagreb',
-      country: 'HR',
-      lat: 22,
-      lng: 33
-    },
-    twitter: '#nodeschool-zagreb'
+  var team = {
+    slug: 'zagreb',
+    name: 'Zagreb',
+    members: [],
+    repos: [{
+      name: 'zagreb'
+    }]
   }
+  var events
+  var chapterRaw
+  var chapter
 
   beforeEach(function () {
+    events = {'a': {
+      location: {
+        name: 'Fashion house',
+        lat: 20,
+        lng: 20
+      },
+      startDate: '2016/02/12',
+      startTime: '20:30',
+      endDate: '2016/02/12',
+      endTime: '22:30',
+      contact: {
+        email: 'm@m.com'
+      },
+      url: 'https://nodeschool.io'
+    }}
+    chapterRaw = {
+      name: 'NodeSchool Zagreb',
+      location: {
+        name: 'Zagreb',
+        country: 'HR',
+        lat: 22,
+        lng: 33
+      },
+      twitter: '#nodeschool-zagreb'
+    }
+    chapter = Object.assign({
+      region: 'Europe',
+      slug: 'zagreb',
+      events: {},
+      repo: 'https://github.com/nodeschool/zagreb',
+      website: 'https://nodeschool.io/zagreb'
+    }, chapterRaw)
     sandbox = sinon.sandbox.create()
   })
 
@@ -36,14 +68,9 @@ describe('Standard List', () => {
   })
 
   it('should get all the repos with new structure', () => {
-    sandbox.stub(Github, 'all', () => {
-      return new Promise(resolve => {
-        resolve([chapter])
-      })
-    })
     sandbox.stub(Json, 'fromUrl', function () {
       return new Promise(resolve => {
-        resolve(chapter)
+        resolve([team])
       })
     })
     sandbox.stub(Validate, 'chapter', function () {
@@ -56,16 +83,52 @@ describe('Standard List', () => {
         resolve(true)
       })
     })
+    sandbox.stub(List, 'downloadChapters', function () {
+      return new Promise(resolve => {
+        resolve([chapter])
+      })
+    })
     return List.run(input).then(chapters => {
-      var chapter = chapters['NodeSchool Zagreb']
-      return expect(chapter.twitter).to.equal('#nodeschool-zagreb')
+      var zagreb = chapters.find(function (chapter) {
+        return chapter.name === 'NodeSchool Zagreb'
+      })
+      return expect(zagreb.twitter).to.equal('#nodeschool-zagreb')
+    })
+  })
+
+  it('should download the details of all chapters', () => {
+    sandbox.stub(List, 'downloadChapter', function () {
+      return new Promise(resolve => {
+        resolve(chapter)
+      })
+    })
+    return List.downloadChapters({}, ['zagreb']).then(chapters => {
+      return expect(chapters[0])
+              .to.be.deep.equals(chapter)
+    })
+  })
+
+  it('should download the events of a chapters', () => {
+    sandbox.stub(List, 'downloadChapterJson', function () {
+      return new Promise(resolve => {
+        resolve(chapterRaw)
+      })
+    })
+    sandbox.stub(Json, 'fromUrl', function () {
+      return new Promise(resolve => {
+        resolve(events)
+      })
+    })
+    return List.downloadChapter({}, 'zagreb').then(zagreb => {
+      return expect(zagreb.events)
+              .to.be.deep.equals(events)
     })
   })
 
   it('should download a chapter', () => {
-    sandbox.stub(List, 'downloadChapterJson', function () {
+    sandbox.stub(Json, 'fromUrl', function () {
       return new Promise(resolve => {
-        resolve(chapter)
+        resolve(chapterRaw)
       })
     })
     sandbox.stub(List, 'downloadEventsJson', function () {
@@ -73,13 +136,18 @@ describe('Standard List', () => {
         resolve({})
       })
     })
-    return List.downloadChapter({}, 'Zagreb').then(chapter => {
-      return expect(chapter)
-              .to.have.all.keys('events', 'location', 'name', 'organizers', 'repo', 'twitter', 'website')
+    return List.downloadChapter({}, 'zagreb').then(zagreb => {
+      return expect(zagreb)
+              .to.be.deep.equals(chapter)
     })
   })
 
   it('should check chapter timezone', () => {
+    sandbox.stub(List, 'downloadChapters', function () {
+      return new Promise(resolve => {
+        resolve([chapter])
+      })
+    })
     sandbox.stub(List, 'downloadChapterJson', function () {
       return new Promise(resolve => {
         resolve(chapter)
@@ -90,7 +158,7 @@ describe('Standard List', () => {
         resolve({})
       })
     })
-    return List.downloadChapter({}, 'Zagreb').then(chapter => {
+    return List.downloadChapter({}, 'zagreb').then(chapter => {
       return expect(chapter)
               .to.have.deep.property('location.timezone', 'Africa/Khartoum')
     })
